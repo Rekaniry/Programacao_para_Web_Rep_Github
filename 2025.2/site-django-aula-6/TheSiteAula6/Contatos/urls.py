b08168ffef566e1c9457b4ac2182615cedd4f636
Felipe Falcão <78935657+Rekaniry@users.noreply.github.com>
2025-09-09 11:49:25 +0000

    Cheio de erro

--- 2025.2/site-django-aula-6/TheSiteAula6/Contatos/urls.py
+++ 2025.2/site-django-aula-6/TheSiteAula6/Contatos/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
+from Contatos import views
+
+app_name = 'Contatos'
 
 urlpatterns = [
-
+    path('', views.ContatoListView.as_view(), name='home-contatos'),
+    path('lista/', views.ContatoListView.as_view(), name='lista-contatos'),
 ]
--- 2025.2/site-django-aula-6/TheSiteAula6/Contatos/views.py
+++ 2025.2/site-django-aula-6/TheSiteAula6/Contatos/views.py
@@ -1,14 +1,45 @@
 from django.shortcuts import render
-from Contatos.models import Pessoa
 from django.views.generic.base import View
+from django.http.response import HttpResponseRedirect
+from django.urls.base import reverse_lazy
+
+class ContatoCreateView(View):
+    def get(self, request, *args, **kwargs):
+        contexto = { 'formulario': ContatoModel2Form, }
+
+        return render(request, "contatos/criaContato.html", contexto)
+
+    def post(self, request, *args, **kwargs):
+        formulario = ContatoModel2Form(request.POST)
+
+        if formulario.is_valid():
+            contato = formulario.save()
+            contato.save()
+
+        return HttpResponseRedirect(reverse_lazy("contatos:lista-contatos"))
 
 class ContatoListView(View):
     def get(self, request, *args, **kwargs):
-        pessoas = Pessoa.objects.all()
+        # Recupera todas as pessoas do banco de dados
+        pessoas = Pessoa.objects.all().order_by('nome')
+
+        # Contexto para o template
+        # Valor da chave é o objeto com todas as pessoas = { Chave 'pessoas': Dicionário contexto }
         contexto = { 'pessoas': pessoas, }
 
         return render(
             request,
-            'contatos/listaContatos.html',
+            'Contatos/listaContatos.html',
             contexto
         )
+
+class ContatoModel2Form(forms.ModelForm):
+    dtNasc = forms.DateField(
+        input_formats=['%d/%m/%Y'],
+        label='Data do nascimento',
+        help_text='Nascimento no formato DD/MM/AAAA',
+    )
+
+class Meta:
+    model = Pessoa
+    fields = '__all__'
